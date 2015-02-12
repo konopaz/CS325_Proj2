bbf1f004ac6d795ab23debf61d5eecb043e6d2f6
Zac Konopa <konopaz@onid.oregonstate.edu>
2015-02-11 22:04:09 -0800

    All three algos and some unit tests for each.

--- change.py
+++ change.py
@@ -1,7 +1,5 @@
 def changeslow(coins, amount, change = None):
 
-  #print amount
-
   if change == None:
     change = []
     for i in range(0, len(coins)):
@@ -21,9 +19,67 @@ def changeslow(coins, amount, change = None):
         iChange = changeslow(coins, i)
         kMinusIChange = changeslow(coins, amount - i)
 
-        #print str(i) + ": " + str(amount)
-        #print iChange
-        #print kMinusIChange
+        tmpCoins2 = 0
+        tmpChange = []
+
+        for i in range(0, len(iChange)):
+          tmpCoins2 = tmpCoins2 + iChange[i]
+          tmpCoins2 = tmpCoins2 + kMinusIChange[i]
+          tmpChange.append(iChange[i] + kMinusIChange[i])
+
+        if tmpCoins == None or tmpCoins2 < tmpCoins:
+          tmpCoins = tmpCoins2
+          change = tmpChange
+
+  return change
+
+def changegreedy(coins, amount):
+
+  change = []
+  for i in range(0, len(coins)):
+    change.append(0)
+
+  coinIdx = len(coins) - 1;
+
+  while amount > 0:
+
+    while coins[coinIdx] > amount:
+      coinIdx = coinIdx - 1
+
+    amount = amount - coins[coinIdx]
+    change[coinIdx] = change[coinIdx] + 1
+
+  return change
+
+def changedp(coins, amount, change = None, table = None):
+
+  if change == None:
+    change = []
+    for i in range(0, len(coins)):
+      change.append(0)
+
+  if table == None:
+    table = {}
+    table[0] = []
+    for i in range(0, len(coins)):
+      table[0].append(0)
+
+  if table.has_key(amount):
+    return table[amount]
+
+  try:
+
+    idx = coins.index(amount)
+    change[idx] = change[idx] + 1
+
+  except ValueError:
+
+    tmpCoins = None
+
+    for i in range(1, amount):
+
+        iChange = changedp(coins, i, None, table)
+        kMinusIChange = changedp(coins, amount - i, None, table)
 
         tmpCoins2 = 0
         tmpChange = []
@@ -36,8 +92,6 @@ def changeslow(coins, amount, change = None):
         if tmpCoins == None or tmpCoins2 < tmpCoins:
           tmpCoins = tmpCoins2
           change = tmpChange
-          #print change
 
-
-
+  table[amount] = change
   return change
--- changetest.py
+++ changetest.py
@@ -5,23 +5,106 @@ import change
 
 class TestChange(unittest.TestCase):
 
+  def test_changedp(self):
+
+    # Test single coin change
+    self.assertEqual(change.changedp([1], 1), [1])
+    self.assertEqual(change.changedp([1, 5], 5), [0, 1])
+    self.assertEqual(change.changedp([1, 5, 10], 10), [0, 0, 1])
+    self.assertEqual(change.changedp([1, 5, 10, 25], 25), [0, 0, 0, 1])
+
+    # Test 1 cent change
+    self.assertEqual(change.changedp([1], 2), [2])
+    self.assertEqual(change.changedp([1], 3), [3])
+    self.assertEqual(change.changedp([1], 4), [4])
+
+    # Test Nickel and Penny
+    self.assertEqual(change.changedp([1, 5], 1), [1, 0])
+    self.assertEqual(change.changedp([1, 5], 2), [2, 0])
+    self.assertEqual(change.changedp([1, 5], 3), [3, 0])
+    self.assertEqual(change.changedp([1, 5], 4), [4, 0])
+    self.assertEqual(change.changedp([1, 5], 5), [0, 1])
+    self.assertEqual(change.changedp([1, 5], 6), [1, 1])
+    self.assertEqual(change.changedp([1, 5], 7), [2, 1])
+    self.assertEqual(change.changedp([1, 5], 8), [3, 1])
+    self.assertEqual(change.changedp([1, 5], 9), [4, 1])
+    self.assertEqual(change.changedp([1, 5], 10), [0, 2])
+    self.assertEqual(change.changedp([1, 5], 11), [1, 2])
+    
+
+    self.assertEqual(change.changedp([1, 3, 7], 1), [1, 0, 0])
+    self.assertEqual(change.changedp([1, 3, 7], 2), [2, 0, 0])
+    self.assertEqual(change.changedp([1, 3, 7], 3), [0, 1, 0])
+    self.assertEqual(change.changedp([1, 3, 7], 4), [1, 1, 0])
+    self.assertEqual(change.changedp([1, 3, 7], 6), [0, 2, 0])
+    self.assertEqual(change.changedp([1, 3, 7], 7), [0, 0, 1])
+    self.assertEqual(change.changedp([1, 3, 7], 8), [1, 0, 1])
+    self.assertEqual(change.changedp([1, 3, 7], 9), [2, 0, 1])
+    self.assertEqual(change.changedp([1, 3, 7], 10), [0, 1, 1])
+
+    self.assertEqual(change.changedp([1, 3, 7], 15), [1, 0, 2])
+    self.assertEqual(change.changedp([1, 3, 7, 26], 22), [1, 0, 3, 0])
+
+    # Misc examples from assignment
+    self.assertEqual(change.changedp([1, 2, 5], 10), [0, 0, 2])
+
+  def test_changegreedy(self):
+
+    # Test single coin change
+    self.assertEqual(change.changegreedy([1], 1), [1])
+    self.assertEqual(change.changegreedy([1, 5], 5), [0, 1])
+    self.assertEqual(change.changegreedy([1, 5, 10], 10), [0, 0, 1])
+    self.assertEqual(change.changegreedy([1, 5, 10, 25], 25), [0, 0, 0, 1])
+
+    # Test 1 cent change
+    self.assertEqual(change.changegreedy([1], 2), [2])
+    self.assertEqual(change.changegreedy([1], 3), [3])
+    self.assertEqual(change.changegreedy([1], 4), [4])
+
+    # Test Nickel and Penny
+    self.assertEqual(change.changegreedy([1, 5], 1), [1, 0])
+    self.assertEqual(change.changegreedy([1, 5], 2), [2, 0])
+    self.assertEqual(change.changegreedy([1, 5], 3), [3, 0])
+    self.assertEqual(change.changegreedy([1, 5], 4), [4, 0])
+    self.assertEqual(change.changegreedy([1, 5], 5), [0, 1])
+    self.assertEqual(change.changegreedy([1, 5], 6), [1, 1])
+    self.assertEqual(change.changegreedy([1, 5], 7), [2, 1])
+    self.assertEqual(change.changegreedy([1, 5], 8), [3, 1])
+    self.assertEqual(change.changegreedy([1, 5], 9), [4, 1])
+    self.assertEqual(change.changegreedy([1, 5], 10), [0, 2])
+    self.assertEqual(change.changegreedy([1, 5], 11), [1, 2])
+    
+
+    self.assertEqual(change.changegreedy([1, 3, 7], 1), [1, 0, 0])
+    self.assertEqual(change.changegreedy([1, 3, 7], 2), [2, 0, 0])
+    self.assertEqual(change.changegreedy([1, 3, 7], 3), [0, 1, 0])
+    self.assertEqual(change.changegreedy([1, 3, 7], 4), [1, 1, 0])
+    self.assertEqual(change.changegreedy([1, 3, 7], 6), [0, 2, 0])
+    self.assertEqual(change.changegreedy([1, 3, 7], 7), [0, 0, 1])
+    self.assertEqual(change.changegreedy([1, 3, 7], 8), [1, 0, 1])
+    self.assertEqual(change.changegreedy([1, 3, 7], 9), [2, 0, 1])
+    self.assertEqual(change.changegreedy([1, 3, 7], 10), [0, 1, 1])
+
+    self.assertEqual(change.changegreedy([1, 3, 7], 15), [1, 0, 2])
+    self.assertEqual(change.changegreedy([1, 3, 7, 26], 22), [1, 0, 3, 0])
+
+    # Misc examples from assignment
+    self.assertEqual(change.changegreedy([1, 2, 5], 10), [0, 0, 2])
+
   def test_changeslow(self):
 
     # Test single coin change
-    print "Doing the really simple ones..."
     self.assertEqual(change.changeslow([1], 1), [1])
     self.assertEqual(change.changeslow([1, 5], 5), [0, 1])
     self.assertEqual(change.changeslow([1, 5, 10], 10), [0, 0, 1])
     self.assertEqual(change.changeslow([1, 5, 10, 25], 25), [0, 0, 0, 1])
 
     # Test 1 cent change
-    print "Doing the still really simple ones..."
     self.assertEqual(change.changeslow([1], 2), [2])
     self.assertEqual(change.changeslow([1], 3), [3])
     self.assertEqual(change.changeslow([1], 4), [4])
 
     # Test Nickel and Penny
-    print "Do the knickel and penny tests..."
     self.assertEqual(change.changeslow([1, 5], 1), [1, 0])
     self.assertEqual(change.changeslow([1, 5], 2), [2, 0])
     self.assertEqual(change.changeslow([1, 5], 3), [3, 0])
@@ -35,7 +118,6 @@ class TestChange(unittest.TestCase):
     self.assertEqual(change.changeslow([1, 5], 11), [1, 2])
     
 
-    print "Doing the harder ones..."
     self.assertEqual(change.changeslow([1, 3, 7], 1), [1, 0, 0])
     self.assertEqual(change.changeslow([1, 3, 7], 2), [2, 0, 0])
     self.assertEqual(change.changeslow([1, 3, 7], 3), [0, 1, 0])
@@ -46,7 +128,6 @@ class TestChange(unittest.TestCase):
     self.assertEqual(change.changeslow([1, 3, 7], 9), [2, 0, 1])
     self.assertEqual(change.changeslow([1, 3, 7], 10), [0, 1, 1])
 
-    #print "Doing the slow ones..."
     #self.assertEqual(change.changeslow([1, 3, 7], 15), [1, 0, 2])
 
     # This one takes forever... does it ever finish?
@@ -54,7 +135,6 @@ class TestChange(unittest.TestCase):
     #self.assertEqual(change.changeslow([1, 3, 7, 26], 22), [1, 0, 3, 0])
 
     # Misc examples from assignment
-    print "Doing misc examples..."
     self.assertEqual(change.changeslow([1, 2, 5], 10), [0, 0, 2])
 
     
